e6c24f330d0f2be9ce57f6c651c8fb137ab06cd0
park jun hyoung <gitpdeathwa@yonsei.ac.kr>
2023-12-27 21:56:41 +0900

    initial commit

---
+++ src/main/java/gitp/csatproblembank/CsatProblemBankApplication.java
@@ -0,0 +1,13 @@
+package gitp.csatproblembank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsatProblemBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsatProblemBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gitp/csatproblembank/CsatProblemBankApplicationTests.java
@@ -0,0 +1,13 @@
+package gitp.csatproblembank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsatProblemBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
